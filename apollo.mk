25200627a1b17847ee5856bb33a035485ca88c80
alexanderianblair <alexander.blair@ukaea.uk>
2022-01-31 15:11:53 +0000

    Move Hephaestus libraries from EXTERNAL_FLAGS to ADDITIONAL_LIBS

--- apollo.mk
+++ apollo.mk
@@ -1,7 +1,7 @@
 #
 # Check the existence of the contrib submodules and build accordingly
 #
-MFEM_DIR?=$(APOLLO_DIR)/../mfem_build
+MFEM_DIR?=$(APOLLO_DIR)/../mfem/build
 include $(MFEM_DIR)/config/config.mk
 
 
