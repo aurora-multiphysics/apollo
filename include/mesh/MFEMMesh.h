bc958c3116e4ac93c0231e2eb250049da868e315
Bill Ellis <billellis.07@gmail.com>
2022-04-04 12:30:55 +0100

    Updated meshing functionality, added MOOSE mesh creation within MFEMMesh, added InheritedMFEMMesh class to allow for mfem meshes to be instantiated from new constructors

--- include/mesh/MFEMMesh.h
+++ include/mesh/MFEMMesh.h
@@ -1,4 +1,5 @@
 #pragma once
+<<<<<<< HEAD
 
 #include "mfem.hpp"
 
@@ -18,6 +19,58 @@ class MFEMMesh : public mfem::Mesh {
   void get_connectivity_data();
 
   void get_cell_type();
+=======
+#include "MooseMesh.h"
+#include "mfem.hpp"
+#include "InheritedMFEMMesh.h"
+#include "libmesh/vtk_io.h"
+#include "libmesh/equation_systems.h"
+#include "libmesh/libmesh_config.h"
+#include "libmesh/mesh_base.h"
+#include "libmesh/mesh_input.h"
+#include "libmesh/numeric_vector.h"
+#include "libmesh/system.h"
+#include "libmesh/node.h"
+#include "libmesh/elem.h"
+#include "libmesh/enum_io_package.h"
+#include "libmesh/ignore_warnings.h"
+#include "libmesh/numeric_vector.h"
+
+/**
+ * MFEMMesh
+ */
+class MFEMMesh : public MooseMesh
+{
+public:
+  static InputParameters validParams();
+
+  MFEMMesh(const InputParameters & parameters);
+  
+
+  virtual void buildMesh() override;
+
+  //Builds placeholder mesh when no MOOSE mesh is required
+  void buildDummyMesh();
+  //Builds the corresponding MOOSE mesh
+  void buildRealMesh();
+
+  //Function that maps vtk element types to libmesh element types
+  int map_elems_vtk_to_libmesh(int VTKElemType);
+
+  //Mesh dimension
+  int dim;
+
+  //The object holding our MFEM mesh. Needs renaming as to avoid confusion
+  InheritedMFEMMesh other_mesh;
+  
+  //Boolean value determining whether the user wants the corresponding MOOSE mesh built
+  bool CreateMOOSEMesh;
+
+  virtual std::unique_ptr<MooseMesh> safeClone() const override;
+
+  
+};
+>>>>>>> 44e4f52 (Updated meshing functionality, added MOOSE mesh creation within MFEMMesh, added InheritedMFEMMesh class to allow for mfem meshes to be instantiated from new constructors)
 
   int get_num_nodes();
 
