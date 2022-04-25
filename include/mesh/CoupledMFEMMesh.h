af3d9c2468d2c16440ab8b081a3988da8a3cc7df
Bill Ellis <billellis.07@gmail.com>
2022-04-25 12:52:15 +0100

    Updated mesh src and include file names

--- include/mesh/CoupledMFEMMesh.h
+++ include/mesh/CoupledMFEMMesh.h
@@ -8,54 +8,49 @@
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #pragma once
+#include "MFEMMesh.h"
 #include "MooseMesh.h"
-#include "mfem.hpp"
-#include "InheritedMFEMMesh.h"
-#include "libmesh/vtk_io.h"
+#include "libmesh/elem.h"
+#include "libmesh/enum_io_package.h"
 #include "libmesh/equation_systems.h"
+#include "libmesh/ignore_warnings.h"
 #include "libmesh/libmesh_config.h"
 #include "libmesh/mesh_base.h"
 #include "libmesh/mesh_input.h"
+#include "libmesh/node.h"
 #include "libmesh/numeric_vector.h"
 #include "libmesh/system.h"
-#include "libmesh/node.h"
-#include "libmesh/elem.h"
-#include "libmesh/enum_io_package.h"
-#include "libmesh/ignore_warnings.h"
-#include "libmesh/numeric_vector.h"
+#include "libmesh/vtk_io.h"
+#include "mfem.hpp"
 
 /**
- * MFEMMesh
+ * CoupledMFEMMesh
  */
-class MFEMMesh : public MooseMesh
-{
-public:
+class CoupledMFEMMesh : public MooseMesh {
+ public:
   static InputParameters validParams();
 
-  MFEMMesh(const InputParameters & parameters);
-  
+  CoupledMFEMMesh(const InputParameters& parameters);
 
   virtual void buildMesh() override;
 
-  //Builds placeholder mesh when no MOOSE mesh is required
+  // Builds placeholder mesh when no MOOSE mesh is required
   void buildDummyMesh();
-  //Builds the corresponding MOOSE mesh
+  // Builds the corresponding MOOSE mesh
   void buildRealMesh();
 
-  //Function that maps vtk element types to libmesh element types
+  // Function that maps vtk element types to libmesh element types
   int map_elems_vtk_to_libmesh(int VTKElemType);
 
-  //Mesh dimension
+  // Mesh dimension
   int dim;
 
-  //The object holding our MFEM mesh. Needs renaming as to avoid confusion
-  InheritedMFEMMesh other_mesh;
-  
-  //Boolean value determining whether the user wants the corresponding MOOSE mesh built
+  // The object holding our MFEM mesh. Needs renaming as to avoid confusion
+  MFEMMesh other_mesh;
+
+  // Boolean value determining whether the user wants the corresponding MOOSE
+  // mesh built
   bool CreateMOOSEMesh;
 
   virtual std::unique_ptr<MooseMesh> safeClone() const override;
-
-  
 };
-
