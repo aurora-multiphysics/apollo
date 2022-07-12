dae799ffcec59015da813cb884266323b3219ed3
alexanderianblair <alexander.i.blair@gmail.com>
2022-07-12 19:20:20 +0000

    Adds MFEMVectorFunctionDirichlet BC

--- include/bcs/MFEMVectorFunctionDirichletBC.h
+++ include/bcs/MFEMVectorFunctionDirichletBC.h
@@ -14,11 +14,12 @@ public:
   virtual ~MFEMVectorFunctionDirichletBC();
 
   virtual hephaestus::BoundaryCondition * getBC() override;
+  virtual void storeCoefficients(hephaestus::DomainProperties & domain_properties) override;
   virtual void execute() override {}
   virtual void initialize() override {}
   virtual void finalize() override {}
 
 protected:
   const Function & _func;
-  mfem::FunctionCoefficient _function_coeff;
+  mfem::VectorFunctionCoefficient _vec_function_coef;
 };
