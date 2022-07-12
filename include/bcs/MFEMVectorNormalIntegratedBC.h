68f471f01e974b5d78d151aecf14db64968d8d0f
alexanderianblair <alexander.i.blair@gmail.com>
2022-07-12 21:16:47 +0000

    Adds MFEMVectorNormalIntegratedBC

---
+++ include/bcs/MFEMVectorNormalIntegratedBC.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "MFEMBoundaryCondition.h"
+#include "boundary_conditions.hpp"
+
+libMesh::Point PointFromMFEMVector(const mfem::Vector & vec);
+
+class MFEMVectorNormalIntegratedBC : public MFEMBoundaryCondition
+{
+public:
+  static InputParameters validParams();
+
+  MFEMVectorNormalIntegratedBC(const InputParameters & parameters);
+  virtual ~MFEMVectorNormalIntegratedBC();
+
+  virtual hephaestus::BoundaryCondition * getBC() override;
+  virtual void storeCoefficients(hephaestus::DomainProperties & domain_properties) override;
+  virtual void execute() override {}
+  virtual void initialize() override {}
+  virtual void finalize() override {}
+
+protected:
+  const Function & _func;
+  mfem::VectorFunctionCoefficient _vec_function_coef;
+};
---
+++ src/bcs/MFEMVectorNormalIntegratedBC.C
@@ -0,0 +1,45 @@
+#include "MFEMVectorNormalIntegratedBC.h"
+#include "Function.h"
+
+registerMooseObject("ApolloApp", MFEMVectorNormalIntegratedBC);
+
+InputParameters
+MFEMVectorNormalIntegratedBC::validParams()
+{
+  InputParameters params = MFEMBoundaryCondition::validParams();
+  params.addParam<FunctionName>(
+      "function", 0, "The vector function to whose normal component is used in the integrated BC");
+  return params;
+}
+
+// TODO: Currently assumes the vector function coefficient is 3D
+MFEMVectorNormalIntegratedBC::MFEMVectorNormalIntegratedBC(const InputParameters & parameters)
+  : MFEMBoundaryCondition(parameters),
+    _func(getFunction("function")),
+    _vec_function_coef(3, [&](const mfem::Vector & p, double t, mfem::Vector & u) {
+      libMesh::RealVectorValue vector_value = _func.vectorValue(t, PointFromMFEMVector(p));
+      u[0] = vector_value(0);
+      u[1] = vector_value(1);
+      u[2] = vector_value(2);
+    })
+{
+  _boundary_condition =
+      new hephaestus::IntegratedBC(getParam<std::string>("variable"),
+                                   bdr_attr,
+                                   new mfem::BoundaryNormalLFIntegrator(_vec_function_coef));
+}
+
+hephaestus::BoundaryCondition *
+MFEMVectorNormalIntegratedBC::getBC()
+{
+  return _boundary_condition;
+}
+
+void
+MFEMVectorNormalIntegratedBC::storeCoefficients(hephaestus::DomainProperties & domain_properties)
+{
+  std::string vec_coef_name = std::string("bc_vec_coef_") + getParam<std::string>("_object_name");
+  domain_properties.vector_property_map[vec_coef_name] = &_vec_function_coef;
+}
+
+MFEMVectorNormalIntegratedBC::~MFEMVectorNormalIntegratedBC() {}
