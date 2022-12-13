5dfdb2c97ea7f946315bb760c06974646a1dd042
Alexander Blair <alexander.blair@ukaea.uk>
2022-12-13 16:36:24 +0000

    Split out MFEMDivFreeVolumetricSource from MFEMSource

---
+++ include/userobjects/MFEMDivFreeVolumetricSource.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "MFEMSource.h"
+
+class MFEMDivFreeVolumetricSource : public MFEMSource
+{
+public:
+  static InputParameters validParams();
+
+  MFEMDivFreeVolumetricSource(const InputParameters & parameters);
+  virtual ~MFEMDivFreeVolumetricSource();
+
+  virtual void execute() override {}
+  virtual void initialize() override {}
+  virtual void finalize() override {}
+
+  virtual hephaestus::Source * getSource() override;
+  virtual void storeCoefficients(hephaestus::DomainProperties & domain_properties) override;
+
+protected:
+  const Function & _func;
+  mfem::VectorFunctionCoefficient _vec_function_coef;
+  mfem::Array<mfem::VectorCoefficient *> sourcecoefs;
+  mfem::Array<int> coilsegments;
+  std::string source_coef_name;
+
+  mfem::PWVectorCoefficient * _restricted_coef;
+};
--- src/userobjects/MFEMSource.C
+++ src/userobjects/MFEMSource.C
@@ -1,5 +1,4 @@
 #include "MFEMSource.h"
-#include "Function.h"
 
 registerMooseObject("ApolloApp", MFEMSource);
 
@@ -7,40 +6,14 @@ InputParameters
 MFEMSource::validParams()
 {
   InputParameters params = GeneralUserObject::validParams();
-  params.addParam<FunctionName>(
-      "function", 0, "The vector function providing the source, to be divergence cleaned.");
   params.addParam<std::vector<SubdomainID>>(
-      "block", "The list of blocks (ids or names) that this object will be applied");
+      "block", "The list of block IDs on which the source will be applied");
   return params;
 }
 
 MFEMSource::MFEMSource(const InputParameters & parameters)
-  : GeneralUserObject(parameters),
-    blocks(getParam<std::vector<SubdomainID>>("block")),
-    _func(getFunction("function")),
-    _vec_function_coef(3,
-                       [&](const mfem::Vector & p, double t, mfem::Vector & u) {
-                         libMesh::RealVectorValue vector_value =
-                             _func.vectorValue(t, PointFromMFEMVector(p));
-                         u[0] = vector_value(0);
-                         u[1] = vector_value(1);
-                         u[2] = vector_value(2);
-                       }),
-    sourcecoefs(blocks.size()),
-    coilsegments(blocks.size())
+  : GeneralUserObject(parameters), blocks(getParam<std::vector<SubdomainID>>("block"))
 {
-  for (unsigned int i = 0; i < blocks.size(); i++)
-  {
-    sourcecoefs[i] = &_vec_function_coef;
-    coilsegments[i] = int(blocks[i]);
-  }
-  _restricted_coef = new mfem::PWVectorCoefficient(3, coilsegments, sourcecoefs);
-
-  hephaestus::InputParameters div_free_source_params;
-  div_free_source_params.SetParam("SourceName", std::string("source"));
-  div_free_source_params.SetParam("HCurlFESpaceName", std::string("_HCurlFESpace"));
-  div_free_source_params.SetParam("H1FESpaceName", std::string("_H1FESpace"));
-  _source = new hephaestus::DivFreeVolumetricSource(div_free_source_params);
 }
 
 hephaestus::Source *
@@ -52,9 +25,6 @@ MFEMSource::getSource()
 void
 MFEMSource::storeCoefficients(hephaestus::DomainProperties & domain_properties)
 {
-  std::string coef_name = std::string("Source");
-  domain_properties.vector_property_map["unblockedsource"] = &_vec_function_coef;
-  domain_properties.vector_property_map["source"] = _restricted_coef;
 }
 
 MFEMSource::~MFEMSource() {}
