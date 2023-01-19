571f28ccef3a7cedd1db53248fe0e883b8da8a94
Alexander Blair <alexander.blair@ukaea.uk>
2023-01-19 22:56:34 +0000

    Update Hephaestus with potential source

---
+++ include/userobjects/MFEMScalarPotentialSource.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "MFEMSource.h"
+
+class MFEMScalarPotentialSource : public MFEMSource
+{
+public:
+  static InputParameters validParams();
+
+  MFEMScalarPotentialSource(const InputParameters & parameters);
+  virtual ~MFEMScalarPotentialSource();
+
+  virtual void execute() override {}
+  virtual void initialize() override {}
+  virtual void finalize() override {}
+
+  virtual hephaestus::Source * getSource() override;
+  virtual void storeCoefficients(hephaestus::DomainProperties & domain_properties) override;
+
+protected:
+  std::string source_coef_name;
+  std::string potential_name;
+  std::string conductivity_coef_name;
+
+  mfem::PWVectorCoefficient * _restricted_coef;
+};
