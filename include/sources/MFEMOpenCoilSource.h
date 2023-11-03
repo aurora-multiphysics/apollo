f34fb965be12a95c7960890ada39a6ca86da8224
Alexander Blair <alexander.blair@ukaea.uk>
2023-11-03 18:59:49 +0000

    Add MFEMOpenCoilSource with example

---
+++ include/sources/MFEMOpenCoilSource.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "MFEMSource.h"
+#include "MFEMVariable.h"
+#include "MFEMCoefficient.h"
+
+class MFEMOpenCoilSource : public MFEMSource
+{
+public:
+  static InputParameters validParams();
+
+  MFEMOpenCoilSource(const InputParameters & parameters);
+  virtual ~MFEMOpenCoilSource();
+
+  virtual void execute() override {}
+  virtual void initialize() override {}
+  virtual void finalize() override {}
+
+  virtual hephaestus::Source * getSource() override;
+  virtual void storeCoefficients(hephaestus::Coefficients & coefficients) override;
+
+protected:
+  const MFEMVariable & _source_current_density_gridfunction;
+  const MFEMVariable & _source_potential_gridfunction;  
+  const MFEMCoefficient & _total_current_coef;
+  const hephaestus::InputParameters _open_coil_params;
+  mfem::Array<int> _coil_domains;
+  const int _coil_in_id, _coil_out_id;
+  const std::pair<int,int> _electrodes;
+};
---
+++ src/sources/MFEMOpenCoilSource.C
@@ -0,0 +1,62 @@
+#include "MFEMOpenCoilSource.h"
+
+registerMooseObject("ApolloApp", MFEMOpenCoilSource);
+
+InputParameters
+MFEMOpenCoilSource::validParams()
+{
+  InputParameters params = MFEMSource::validParams();
+
+  params.addParam<UserObjectName>(
+      "total_current_coef",
+      "The total current ($I$) flowing through the coil. May be time dependent.");
+
+  params.addParam<UserObjectName>("source_current_density_gridfunction",
+                                  "The gridfunction to store the source current density.");
+
+  params.addParam<UserObjectName>("source_potential_gridfunction",
+                                  "The gridfunction to store the scalar potential in the coil.");
+
+  params.addParam<BoundaryName>(
+      "coil_in_boundary",
+      "The boundary (id or name) from the mesh determinining the coil input terminal");
+
+  params.addParam<BoundaryName>(
+      "coil_out_boundary",
+      "The boundary (id or name) from the mesh determinining the coil output terminal");
+  return params;
+}
+
+MFEMOpenCoilSource::MFEMOpenCoilSource(const InputParameters & parameters)
+  : MFEMSource(parameters),
+    _source_current_density_gridfunction(
+        getUserObject<MFEMVariable>("source_current_density_gridfunction")),
+    _source_potential_gridfunction(getUserObject<MFEMVariable>("source_potential_gridfunction")),
+    _total_current_coef(getUserObject<MFEMCoefficient>("total_current_coef")),
+    _open_coil_params({{"SourceName", _source_current_density_gridfunction.name()},
+                       {"PotentialName", _source_potential_gridfunction.name()},
+                       {"IFuncCoefName", _total_current_coef.name()}}),
+    _coil_domains(blocks.size()),
+    _coil_in_id(std::stoi(getParam<BoundaryName>("coil_in_boundary"))),
+    _coil_out_id(std::stoi(getParam<BoundaryName>("coil_out_boundary"))),
+    _electrodes(std::make_pair(_coil_in_id, _coil_out_id))
+{
+  for (unsigned int bid = 0; bid < blocks.size(); ++bid)
+  {
+    _coil_domains[bid] = blocks[bid];
+  }
+  _source = new hephaestus::OpenCoilSolver(_open_coil_params, _coil_domains, _electrodes);
+}
+
+hephaestus::Source *
+MFEMOpenCoilSource::getSource()
+{
+  return _source;
+}
+
+void
+MFEMOpenCoilSource::storeCoefficients(hephaestus::Coefficients & coefficients)
+{
+}
+
+MFEMOpenCoilSource::~MFEMOpenCoilSource() {}
