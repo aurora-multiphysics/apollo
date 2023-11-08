fbf2ded977903685aefe42f4f565f12bcebd1748
Edward Palmer <edward.palmer@ukaea.uk>
2023-11-08 16:20:22 +0000

    Added a V2 version of the AuxKernel which is derived from VectorAuxKernel. This means the user no longer needs to provide a dummy variable.

---
+++ include/auxkernels/VectorVariableComponentsAuxV2.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "AuxKernel.h"
+
+/**
+ * Construct a vector variable from 3 standard variables.
+ */
+class VectorVariableComponentsAuxV2 : public VectorAuxKernel
+{
+public:
+  static InputParameters validParams();
+
+  VectorVariableComponentsAuxV2(const InputParameters & parameters);
+
+protected:
+  virtual void compute() override final;
+
+  // NB: not used.
+  virtual RealVectorValue computeValue() override final { mooseError("Unused"); }
+
+  /**
+   * The component variables. These are set internally from the vector variable.
+   */
+  MooseVariable & _component_x;
+  MooseVariable & _component_y;
+  MooseVariable & _component_z;
+
+private:
+  /**
+   * Returns a *writable* MooseVariable object for a nodal or elemental variable. Use
+   * var.setNodalValue(val[, idx]) in both cases (!) to set the solution DOF values. See
+   * identically-named method in "Coupleable.h". The reason for this version is that
+   * the latter method refuses to work with VectorAuxKernels.
+   */
+  MooseVariable & writableVariable(const std::string & var_name, unsigned int comp = 0);
+};
