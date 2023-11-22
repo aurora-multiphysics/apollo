3032b8a988ac7f6289836103f5616f9be7222fee
Edward Palmer <edward.palmer@ukaea.uk>
2023-11-22 15:49:51 +0000

    Added initial methods (some not implemented) for the vector transfer action.

---
+++ include/actions/AddVectorTransferAction.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "MooseObjectAction.h"
+
+class AddVectorTransferAction : public MooseObjectAction
+{
+public:
+  static InputParameters validParams();
+
+  AddVectorTransferAction(const InputParameters & params);
+
+  virtual void act() override;
+
+protected:
+  const std::vector<VariableName> & getFromVarNames() const;
+  const std::vector<AuxVariableName> & getToVarNames() const;
+
+  /**
+   * Returns a shared pointer to or from the multiapp.
+   */
+  const std::shared_ptr<MultiApp> getFromMultiApp() const;
+  const std::shared_ptr<MultiApp> getToMultiApp() const;
+
+  /**
+   * Finds the FEProblemBase depending on the direction.
+   */
+  FEProblemBase & getFromProblem() const;
+  FEProblemBase & getToProblem() const;
+
+  void initializeMultiApps();
+
+private:
+  /**
+   * An enumeration used internally to specify the component of a vector variable.
+   */
+  enum class VectorComponent
+  {
+    X,
+    Y,
+    Z
+  };
+
+  /**
+   * An enumeration used to specify which auxkernel to build.
+   */
+  enum class VectorAuxKernelType
+  {
+    PREPARE_VECTOR_FOR_TRANSFER,
+    RECOVER_VECTOR_POST_TRANSFER
+  };
+
+  /**
+   * MultiApps the transfer is transferring data to or from.
+   */
+  std::shared_ptr<MultiApp> _from_multi_app;
+  std::shared_ptr<MultiApp> _to_multi_app;
+};
---
+++ src/actions/AddVectorTransferAction.C
@@ -0,0 +1,99 @@
+#include "AddVectorTransferAction.h"
+#include "FEProblem.h"
+
+registerMooseAction("MooseApp", AddVectorTransferAction, "add_vector_transfer");
+
+InputParameters
+AddVectorTransferAction::validParams()
+{
+  InputParameters params = MooseObjectAction::validParams();
+  params.addClassDescription("Add a Transfer object that can handle vectors to the simulation.");
+  return params;
+}
+
+AddVectorTransferAction::AddVectorTransferAction(const InputParameters & params)
+  : MooseObjectAction(params)
+{
+  initializeMultiApps();
+}
+
+void
+AddVectorTransferAction::act()
+{
+  /**
+   * Okay, we have the MOOSE object parameters. This is good! And we have already created the
+   * parameters for the transfer type that the user has specified. We will need to modify some of
+   * these input parameters.
+   */
+  InputParameters & input_params = getObjectParams();
+
+  // Print source variables.
+  // std::cout << "fromVarNames: ";
+  // for (auto & source_variable : getFromVarNames())
+  // {
+  //   std::cout << source_variable << ", ";
+  // }
+  // std::cout << std::endl;
+
+  // std::cout << "toVarNames: ";
+  // for (auto & source_variable : getToVarNames())
+  // {
+  //   std::cout << source_variable << ", ";
+  // }
+  // std::cout << std::endl;
+
+  // _problem->addTransfer(_type, _name, input_params);
+}
+
+const std::vector<VariableName> &
+AddVectorTransferAction::getFromVarNames() const
+{
+  return getObjectParams().get<std::vector<VariableName>>("source_variable");
+}
+
+const std::vector<AuxVariableName> &
+AddVectorTransferAction::getToVarNames() const
+{
+  return getObjectParams().get<std::vector<AuxVariableName>>("variable");
+}
+
+void
+AddVectorTransferAction::initializeMultiApps()
+{
+  mooseError("Not implemented");
+}
+
+FEProblemBase &
+AddVectorTransferAction::getFromProblem() const
+{
+  mooseError("Not implemented");
+}
+
+FEProblemBase &
+AddVectorTransferAction::getToProblem() const
+{
+  mooseError("Not implemented");
+}
+
+const std::shared_ptr<MultiApp>
+AddVectorTransferAction::getFromMultiApp() const
+{
+  if (!_from_multi_app)
+  {
+    mooseError(
+        "A from_multiapp was requested but is unavailable. Check the from_multi_app parameter");
+  }
+
+  return _from_multi_app;
+}
+
+const std::shared_ptr<MultiApp>
+AddVectorTransferAction::getToMultiApp() const
+{
+  if (!_to_multi_app)
+  {
+    mooseError("A to_multiapp was requested but is unavailable. Check the to_multi_app parameter");
+  }
+
+  return _to_multi_app;
+}
