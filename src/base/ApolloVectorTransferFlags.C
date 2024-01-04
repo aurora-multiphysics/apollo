ba42baae7dd42902f679b54c4e367f2771a3a64a
Edward Palmer <edward.palmer@ukaea.uk>
2024-01-04 17:13:22 +0000

    Added Apollo vector transfer flags.

---
+++ src/base/ApolloVectorTransferFlags.C
@@ -0,0 +1,8 @@
+#include "ApolloVectorTransferFlags.h"
+
+#include "ExecFlagRegistry.h"
+
+const ExecFlagType ApolloApp::EXEC_PREPARE_VECTOR_FOR_TRANSFER =
+    registerExecFlag("PREPARE_VECTOR_FOR_TRANSFER");
+const ExecFlagType ApolloApp::EXEC_RECOVER_VECTOR_POST_TRANSFER =
+    registerExecFlag("RECOVER_VECTOR_POST_TRANSFER");
