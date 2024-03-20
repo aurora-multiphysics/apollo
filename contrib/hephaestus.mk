756c1c4e7c5f5439b651b51e4429b14d429f915f
Alexander Blair <alexander.blair@ukaea.uk>
2024-03-20 20:13:53 +0000

    Revert addition of Hypre explicitly in hephaestus.mk

--- contrib/hephaestus.mk
+++ contrib/hephaestus.mk
@@ -1,15 +1,11 @@
 ############################### HEPHAESTUS ######################################
 #################################################################################
 MFEM_DIR				:=$(APPLICATION_DIR)/../mfem/build
-HYPRE_DIR				:=$(APPLICATION_DIR)/../hypre-2.28.0/src/
 HEPHAESTUS_DIR 			:=$(APPLICATION_DIR)/contrib/hephaestus
 
 include $(MFEM_DIR)/config/config.mk
-MFEM_INCLUDE 			:= -I$(MFEM_DIR) -I$(MFEM_SOURCE_DIR) -I$(MFEM_SOURCE_DIR)/miniapps/common
-MFEM_LIB 				:= $(MFEM_DIR) -L$(MFEM_DIR) -lmfem -lrt $(MFEM_DIR)/miniapps/common/ -L$(MFEM_DIR)/miniapps/common -lmfem-common
-
-HYPRE_INCLUDE 			:= -I$(HYPRE_DIR)
-HYPRE_LIB 				:= -L$(HYPRE_DIR)/lib/ -lHYPRE
+MFEM_INCLUDES 			:= -I$(MFEM_INC_DIR)/config -I$(MFEM_DIR)/ -I$(MFEM_DIR)/../miniapps/common
+MFEM_LIBS 				:= -L$(MFEM_DIR) -lmfem -lrt -L$(MFEM_DIR)/miniapps/common -lmfem-common $(MFEM_LIB)
 
 HEPHAESTUS_LIB_DIR		:= $(HEPHAESTUS_DIR)/lib
 HEPHAESTUS_LIB 			:= $(HEPHAESTUS_LIB_DIR)/libhephaestus.so
@@ -17,8 +13,8 @@ HEPHAESTUS_LIB 			:= $(HEPHAESTUS_LIB_DIR)/libhephaestus.so
 HEPHAESTUS_INCLUDE_LIB	:= $(sort $(dir $(shell find $(HEPHAESTUS_DIR)/src/ -name "*.hpp")))
 HEPHAESTUS_INCLUDE		:= $(foreach d, $(HEPHAESTUS_INCLUDE_LIB), -I$d) -I$(APPLICATION_DIR)/contrib/hephaestus/build/_deps/spdlog-src/include/
 
-HEPHAESTUS_CXX_FLAGS 	:= -Wall $(HEPHAESTUS_INCLUDE) $(MFEM_INCLUDE) -I$(MFEM_INC_DIR)/config $(HYPRE_INCLUDE)
-HEPHAESTUS_LDFLAGS		:= -Wl,-rpath,$(HEPHAESTUS_LIB_DIR) -L$(HEPHAESTUS_LIB_DIR) -lhephaestus $(MFEM_LIB) ${HYPRE_LIB} -L$(APPLICATION_DIR)/contrib/hephaestus/build/_deps/spdlog-build/ -lspdlogd
+HEPHAESTUS_CXX_FLAGS 	:= -Wall $(HEPHAESTUS_INCLUDE) $(MFEM_INCLUDES) -I$(MFEM_INC_DIR)/config
+HEPHAESTUS_LDFLAGS		:= -Wl,-rpath,$(HEPHAESTUS_LIB_DIR) -L$(HEPHAESTUS_LIB_DIR) -lhephaestus $(MFEM_LIBS) -L$(APPLICATION_DIR)/contrib/hephaestus/build/_deps/spdlog-build/ -lspdlogd
 
 libmesh_CXXFlags 		+= $(HEPHAESTUS_CXX_FLAGS)
 libmesh_LDFLAGS 		+= $(HEPHAESTUS_LDFLAGS)
