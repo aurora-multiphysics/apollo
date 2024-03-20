b860a031f1d8c5555d3af2a20f66574811f26ecc
Alexander Blair <alexander.blair@ukaea.uk>
2024-03-20 21:52:45 +0000

    Correct spdlog library name and use ninja as hephaestus build system

--- contrib/hephaestus.mk
+++ contrib/hephaestus.mk
@@ -7,14 +7,16 @@ include $(MFEM_DIR)/config/config.mk
 MFEM_INCLUDES 			:= -I$(MFEM_INC_DIR)/config -I$(MFEM_DIR)/ -I$(MFEM_DIR)/../miniapps/common
 MFEM_LIBS 				:= -L$(MFEM_DIR) -lmfem -lrt -L$(MFEM_DIR)/miniapps/common -lmfem-common $(MFEM_LIB)
 
-HEPHAESTUS_LIB_DIR		:= $(HEPHAESTUS_DIR)/lib
-HEPHAESTUS_LIB 			:= $(HEPHAESTUS_LIB_DIR)/libhephaestus.so
+HEPHAESTUS_LIB		:= $(HEPHAESTUS_DIR)/lib
+
+SPDLOG_LIB 			:=$(HEPHAESTUS_DIR)/build/_deps/spdlog-build/
+SPDLOG_INCLUDE 		:=$(HEPHAESTUS_DIR)/build/_deps/spdlog-src/include/
 
 HEPHAESTUS_INCLUDE_LIB	:= $(sort $(dir $(shell find $(HEPHAESTUS_DIR)/src/ -name "*.hpp")))
-HEPHAESTUS_INCLUDE		:= $(foreach d, $(HEPHAESTUS_INCLUDE_LIB), -I$d) -I$(APPLICATION_DIR)/contrib/hephaestus/build/_deps/spdlog-src/include/
+HEPHAESTUS_INCLUDE		:= $(foreach d, $(HEPHAESTUS_INCLUDE_LIB), -I$d) -I$(SPDLOG_INCLUDE)
 
 HEPHAESTUS_CXX_FLAGS 	:= -Wall $(HEPHAESTUS_INCLUDE) $(MFEM_INCLUDES) -I$(MFEM_INC_DIR)/config
-HEPHAESTUS_LDFLAGS		:= -Wl,-rpath,$(HEPHAESTUS_LIB_DIR) -L$(HEPHAESTUS_LIB_DIR) -lhephaestus $(MFEM_LIBS) -L$(APPLICATION_DIR)/contrib/hephaestus/build/_deps/spdlog-build/ -lspdlogd
+HEPHAESTUS_LDFLAGS		:= -Wl,-rpath,$(HEPHAESTUS_LIB) -L$(HEPHAESTUS_LIB) -lhephaestus $(MFEM_LIBS) -L$(SPDLOG_LIB) -lspdlog
 
 libmesh_CXXFlags 		+= $(HEPHAESTUS_CXX_FLAGS)
 libmesh_LDFLAGS 		+= $(HEPHAESTUS_LDFLAGS)
